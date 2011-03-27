827ca2a20635486f07b142fa8837f321e24e5f27
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2011-03-27 14:48:34 +0000

    moved Delphi2007 projects to subdirectory Delphi2007
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@616 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/projects/regularexpert/__clean.cmd
+++ branches/formatter/trunk/projects/regularexpert/__clean.cmd
@@ -1,3 +1,16 @@
+call :cleandir 2007
+call :doclean
+del GXIcons.res
+pause
+goto :eof
+
+:cleandir
+cd delphi%1
+call :doclean
+cd ..
+goto :eof
+
+:doclean
 del GExperts*.~*
 del GExperts*.bdsproj.local
 del GExperts*.dproj.local
@@ -8,3 +21,4 @@ del /s /q __history\*
 rd __history
 del /s /q ModelSupport\*
 rd ModelSupport
+goto :eof
