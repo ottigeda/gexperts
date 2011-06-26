6b0da6d65011273a2722b685d813b7f6d47d192b
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2011-06-26 16:54:38 +0000

    script for creating a relase archive
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@640 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ branches/formatter/trunk/__Release.cmd
@@ -0,0 +1,25 @@
+set ZIPFile=GExperts-experimental-twm.zip
+
+del %ZIPFile%
+cd release
+zip -9 ..\%ZIPFile% .
+cd ..
+
+call :dozip editorexpert
+call :dozip regularexpert
+
+
+pause
+goto :eof
+
+:dozip
+
+zip -9 %ZIPFile% %1\GExpertsD6.dll
+zip -9 %ZIPFile% %1\GExpertsD7.dll
+zip -9 %ZIPFile% %1\GExpertsDelphi2005.dll
+zip -9 %ZIPFile% %1\GExpertsDBDS2006.dll
+zip -9 %ZIPFile% %1\GExpertsDelphi2007.dll
+zip -9 %ZIPFile% %1\GExpertsRS2009.dll
+zip -9 %ZIPFile% %1\GExpertsRS2010.dll
+zip -9 %ZIPFile% %1\GExpertsRSXE1.dll
+goto :eof
