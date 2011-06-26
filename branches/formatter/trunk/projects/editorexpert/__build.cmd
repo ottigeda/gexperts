848b3679150505ffe12ea61630b5177633deb7de
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2011-06-26 16:01:20 +0000

    scripts for calling all the build scripts
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@636 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ branches/formatter/trunk/projects/editorexpert/__build.cmd
@@ -0,0 +1,31 @@
+pushd Delphi6
+call __Build_Project.cmd
+popd
+
+pushd Delphi7
+call __Build_Project.cmd
+popd
+
+pushd Delphi2005
+call __Build_Project.cmd
+popd
+
+pushd Delphi2006
+call __Build_Project.cmd
+popd
+
+pushd Delphi2007
+call __Build_Project.cmd
+popd
+
+pushd Delphi2009
+call __Build_Project.cmd
+popd
+
+pushd Delphi2010
+call __Build_Project.cmd
+popd
+
+pushd Delphi2011
+call __Build_Project.cmd
+popd
