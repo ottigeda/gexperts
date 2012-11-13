a52863c26555c196c6dcbef15190245621c29acb
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2012-11-13 16:37:40 +0000

    new test case uses with comment
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@757 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ branches/formatter/trunk/source/unittests/testcases/input/testfile_useswithcomment.pas
@@ -0,0 +1,15 @@
+unit testfile_useswithcomment;
+
+interface
+
+uses
+  Sysutils, // comment 1
+  Classes; // comment 2
+  
+implementation
+
+uses
+  Windows, // comment3
+  Controls; // comment4
+
+end.
