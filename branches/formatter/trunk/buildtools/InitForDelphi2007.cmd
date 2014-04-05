615e1ec09ba8ec5a5461ec7d5251f39f2da5531f
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2014-04-05 16:29:32 +0000

    Bugfix for Delphi 2007 under 64 bit Windows
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@806 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/buildtools/InitForDelphi2007.cmd
+++ branches/formatter/trunk/buildtools/InitForDelphi2007.cmd
@@ -1,5 +1,9 @@
 rem @echo off
 echo Initializing for Delphi 2007
 call %~p0%delphiversions.cmd
+set OldPath=%PATH%
 set DelphiExe=%Delphi2007Dir%\bin\bds.exe
 call "%Delphi2007Dir%\bin\rsvars.bat"
+rem Delphi 2007 sets the FrameworkDir incorrectly on 64 bit Windows, so we fix this here
+SET FrameworkDir=%SystemRoot%\Microsoft.NET\Framework\
+SET PATH=%FrameworkDir%%FrameworkVersion%;%FrameworkSDKDir%;%OldPath%
