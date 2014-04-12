6880ae6d1c930a947ec00ba0dc4edb8ce5eba1b3
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2014-04-12 15:39:08 +0000

    Bugfix: __clean.cmd must call the other __clean.cmd in subdir projects
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@836 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/__clean.cmd
+++ branches/formatter/trunk/__clean.cmd
@@ -1,30 +1,3 @@
-if "%1"=="" goto :list
-call :doItem %1
-goto :eof
-:list
-
-call :doItem 6
-call :doItem 7
-call :doItem 2005
-call :doItem 2006
-call :doItem 2007
-call :doItem 2009
-call :doItem 2010
-call :doItem XE1
-call :doItem XE2
-call :doItem XE3
-call :doItem XE4
-call :doItem XE5
-
-goto :eof
-
-:doItem
-pushd dcu
-call __clean.cmd %1
-popd
 pushd projects
-call __clean.cmd %1
+call __clean.cmd
 popd
-pause
-
-goto :eof
