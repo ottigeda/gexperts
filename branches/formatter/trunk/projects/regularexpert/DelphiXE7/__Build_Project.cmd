b35a892fe15658180ddec1342f3f499cab77c258
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2014-09-04 20:33:19 +0000

    adapted to Delphi XE7
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@886 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/projects/regularexpert/DelphiXE7/__Build_Project.cmd
+++ branches/formatter/trunk/projects/regularexpert/DelphiXE7/__Build_Project.cmd
@@ -2,8 +2,8 @@
 @rem Copy this to the project's root directory and
 @rem change the projectname below
 setlocal
-call ..\..\..\buildtools\InitForDelphiXE6.cmd"
-msbuild /target:Build /p:config=Release GExpertsRSXE6.dproj
-echo done building GExpertsRSXE6
+call ..\..\..\buildtools\InitForDelphiXE7.cmd"
+msbuild /target:Build /p:config=Release GExpertsRSXE7.dproj
+echo done building GExpertsRSXE7
 endlocal
 pause
