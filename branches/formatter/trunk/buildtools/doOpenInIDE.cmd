daa51b77204c4eb6d1f6647a7ce61f37431babbc
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-10-04 14:10:02 +0000

    * determining the Delphi version moved to doGetDelphiVer.cmd * directly call delphipath.cmd rather than InitForDelphi%delphiver%.cmd
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@978 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/buildtools/doOpenInIDE.cmd
+++ branches/formatter/trunk/buildtools/doOpenInIDE.cmd
@@ -1,23 +1,10 @@
 @rem Open project in Delphi IDE
-setlocal
 @echo off
-call :GetLastDir %CD%
-set thisdir=%result%
-set delphiver=%thisdir:~6%
-call _prebuild.cmd
-call ..\..\..\buildtools\InitForDelphi%delphiver%.cmd
-start "Delphi %delphiver% IDE" "%DelphiPath%\bin\delphi32.exe" GExpertsD%delphiver%.dpr
-goto :eof
-
-:GetLastDir
-rem extract path
 setlocal
-set directory=%1%
-rem extract "filename" (= last directory of path)
-call :LastItem %directory%
-endlocal & set result=%result%
-goto :eof
-
-:LastItem
-set result=%~n1%
+set buildtools=%~dp0%
+call %buildtools%doGetDelphiVer.cmd
+call _prebuild.cmd
+call %buildtools%\delphipath.cmd %delphiver%
+start "Delphi %delphiver% IDE" "%DelphiPath%\bin\delphi32.exe" GExpertsD%delphiver%.dpr
+endlocal
 goto :eof
