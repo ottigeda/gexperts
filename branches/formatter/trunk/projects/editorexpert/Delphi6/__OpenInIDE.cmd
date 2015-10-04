73a30a08361f0bd97185437ab1e2fbe64a4c8320
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-10-04 14:57:41 +0000

    automatically find the buildtools subdir in one of the parent directories
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@980 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/projects/editorexpert/Delphi6/__OpenInIDE.cmd
+++ branches/formatter/trunk/projects/editorexpert/Delphi6/__OpenInIDE.cmd
@@ -1,2 +1,36 @@
-@rem Open project in Delphi IDE
-call ..\..\..\buildtools\doOpenInIde.cmd
+@rem Searches the parent dirctories for the buildtools and calls the doOpenInIde.cmd there
+@echo off
+setlocal
+call :GetBuildTools %0%
+call %result%\doOpenInIde.cmd
+goto :eof
+
+:GetBuildTools
+@rem search all parent directories for a subdir "buildtools" and return
+@rem the full path to the buildtools directory in %result%
+setlocal
+set parentdir=%1%
+:loop
+call :GetDir %parentdir%
+set parentdir=%result%
+if exist %parentdir%\buildtools goto found
+goto loop
+:found
+set result=%parentdir%\buildtools
+endlocal & set result=%result%
+goto :eof
+
+:GetDir
+rem extract path
+setlocal
+set result=%~dp1%
+rem remove any quotes
+set result=%result:"=%
+rem add quotes
+set result="%result%"
+rem remove \ before the closing quote
+set result=%result:\"="%
+rem remove any quotes
+set result=%result:"=%
+endlocal & set result=%result%
+goto :eof
