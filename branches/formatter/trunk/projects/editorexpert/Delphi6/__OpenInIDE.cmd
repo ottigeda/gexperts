b50a164c747be877984b9909995cabc64da1f7c0
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-10-04 13:42:26 +0000

    moved most of the code to buildtools\doOpenInIDE.cmd
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@976 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/projects/editorexpert/Delphi6/__OpenInIDE.cmd
+++ branches/formatter/trunk/projects/editorexpert/Delphi6/__OpenInIDE.cmd
@@ -1,23 +1,2 @@
 @rem Open project in Delphi IDE
-setlocal
-@echo off
-call :GetLastDir %CD%
-set thisdir=%result%
-set delphiver=%thisdir:~6%
-call _prebuild.cmd
-call ..\..\..\buildtools\InitForDelphi%delphiver%.cmd
-start "Delphi %delphiver% IDE" "%DelphiPath%\bin\delphi32.exe" GExpertsD%delphiver%.dpr
-goto :eof
-
-:GetLastDir
-rem extract path
-setlocal
-set directory=%1%
-rem extract "filename" (= last directory of path)
-call :LastItem %directory%
-endlocal & set result=%result%
-goto :eof
-
-:LastItem
-set result=%~n1%
-goto :eof
+call ..\..\..\buildtools\doOpenInIde.cmd
