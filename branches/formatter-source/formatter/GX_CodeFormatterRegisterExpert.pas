5f674e82ee5726358483ff5c5816a8e8d9010534
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-11-08 13:44:10 +0000

    register expert code (from editor expert)
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@1052 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ branches/formatter-source/formatter/GX_CodeFormatterRegisterExpert.pas
@@ -0,0 +1,14 @@
+unit GX_CodeFormatterRegisterExpert;
+
+interface
+
+implementation
+
+uses
+  GX_EditorExpert,
+  GX_eCodeFormatter;
+
+initialization
+  RegisterEditorExpert(TeCodeFormatterExpert);
+end.
+
