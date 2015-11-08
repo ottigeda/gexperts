1a6287d4d941343f7a9ffe8bc587906f68d119d1
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-11-08 13:44:48 +0000

    updated projects for Delphi 6 and 7
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@1053 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/projects/editorexpert/Delphi6/_prebuild.cmd
+++
@@ -1,3 +0,0 @@
-del ..\..\..\dcu\delphi6\editorexpert\*.dcu
-..\..\..\buildtools\dof2cfg GExpertsD6.dpr
-call ..\..\..\buildtools\prebuild.cmd GExpertsD6.dpr
--- branches/formatter/trunk/projects/editorexpert/Delphi7/_prebuild.cmd
+++
@@ -1,3 +0,0 @@
-del ..\..\..\dcu\delphi7\editorexpert\*.dcu
-..\..\..\buildtools\dof2cfg GExpertsD7.dpr
-call ..\..\..\buildtools\prebuild.cmd GExpertsD7.dpr
