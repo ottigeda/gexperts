6fb53b071b26332f4f7d54ce15a75b86933c99ed
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-12-13 11:09:58 +0000

    call GetAboutIcon
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@1129 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter-source/formatter/GX_AboutExperimental.pas
+++ branches/formatter-source/formatter/GX_AboutExperimental.pas
@@ -24,7 +24,7 @@ type
     l_DummzeuchDe: TLabel;
     l_Formatter: TLabel;
   protected
-    class function doAddToAboutDialog: integer; override;
+    class function DoAddToAboutDialog: integer; override;
   public
     constructor Create(_Owner: TComponent); override;
   end;
@@ -49,18 +49,14 @@ begin
   SetFontColor(l_Formatter, clBlue);
 end;
 
-class function TfmAboutExperimental.doAddToAboutDialog: integer;
+class function TfmAboutExperimental.DoAddToAboutDialog: integer;
 {$IFDEF GX_VER170_up}
 // Only Delphi 2005 and up support the about box services
 var
-  bmSplashScreen: HBITMAP;
   AboutBoxServices: IOTAAboutBoxServices;
-{$ENDIF GX_VER170_up}
 begin
   Result := -1;
-{$IFDEF GX_VER170_up}
   if Supports(BorlandIDEServices, IOTAAboutBoxServices, AboutBoxServices) then begin
-    bmSplashScreen := LoadBitmap(HInstance, 'SplashScreenBitMap');
     Result := AboutBoxServices.AddPluginInfo(
       'GExperts Experimental',
       'GExperts is a free set of tools built to increase the productivity of Delphi and C++Builder'
@@ -68,13 +64,17 @@ begin
       + ' GExperts is developed as Open Source software and we encourage user contributions to the project.'#13#10
       + '(c) 2015 by Thomas Mueller'#13#10
       + 'http://blog.dummzeuch.de',
-      bmSplashScreen,
+      GetAboutIcon,
       False,
       '', // leave this empty!
       GetVersionStr + ' experimental');
   end;
-{$ENDIF GX_VER170_up}
 end;
+{$ELSE not GX_VER170_up}
+begin
+  Result := -1;
+end;
+{$ENDIF not GX_VER170_up}
 
 initialization
   TfmAboutExperimental.SetCustomBuildEmails('gexperts@dummzeuch.de', 'gexperts@dummzeuch.de');
