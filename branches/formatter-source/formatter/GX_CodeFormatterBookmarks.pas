e6cfdb6a59009edaaf55bf82b19a6e87619ab6be
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2015-12-31 17:26:23 +0000

    moved bookmarklist from formatter to Utils
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@1166 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter-source/formatter/GX_CodeFormatterBookmarks.pas
+++ branches/formatter-source/formatter/GX_CodeFormatterBookmarks.pas
@@ -9,37 +9,13 @@ interface
 uses
   SysUtils,
   Classes,
-  ToolsApi;
+  ToolsApi,
+  GX_BookmarkList;
 
 type
-  TBookmark = class
-  private
-    fLine: Integer;
-    fNumber: Integer;
-  public
-    constructor Create(_Number, _Line: Integer);
-    property Number: Integer read fNumber;
-    property Line: Integer read fLine;
-  end;
-
-  TBookmarks = class
-  private
-    function GetCount: Integer;
-    function GetItems(_Idx: Integer): TBookmark;
-  protected
-    fList: TList;
-  public
-    constructor Create;
-    destructor Destroy; override;
-    procedure Add(_Number, _Line: Integer);
-    procedure Clear;
-    property Items[_Idx: Integer]: TBookmark read GetItems; default;
-    property Count: Integer read GetCount;
-  end;
-
   TBookmarkHandler = class
   private
-    FBookmarks: TBookmarks;
+    FBookmarks: TBookmarkList;
   protected
     function GetEditView(var _SourceEditor: IOTASourceEditor; var _EditView: IOTAEditView): boolean;
   public
@@ -54,66 +30,12 @@ implementation
 uses
   GX_OtaUtils;
 
-{ TBookmark }
-
-constructor TBookmark.Create(_Number, _Line: Integer);
-begin
-  inherited Create;
-  fNumber := _Number;
-  fLine := _Line;
-end;
-
-{ TBookmarks }
-
-constructor TBookmarks.Create;
-begin
-  inherited;
-  fList := TList.Create;
-end;
-
-destructor TBookmarks.Destroy;
-var
-  i: Integer;
-begin
-  if Assigned(fList) then begin
-    for i := 0 to fList.Count - 1 do
-      TBookmark(fList[i]).Free;
-    fList.Free;
-    fList := nil;
-  end;
-  inherited;
-end;
-
-procedure TBookmarks.Add(_Number, _Line: Integer);
-begin
-  fList.Add(TBookmark.Create(_Number, _Line))
-end;
-
-function TBookmarks.GetCount: Integer;
-begin
-  Result := fList.Count;
-end;
-
-function TBookmarks.GetItems(_Idx: Integer): TBookmark;
-begin
-  Result := fList[_Idx];
-end;
-
-procedure TBookmarks.Clear;
-var
-  i: Integer;
-begin
-  for i := 0 to fList.Count - 1 do
-    TBookmark(fList[i]).Free;
-  fList.Clear;
-end;
-
 { TBookmarkHandler }
 
 constructor TBookmarkHandler.Create;
 begin
   inherited Create;
-  FBookmarks := TBookmarks.Create;
+  FBookmarks := TBookmarkList.Create;
 end;
 
 destructor TBookmarkHandler.Destroy;
@@ -145,7 +67,7 @@ begin
   for i := 0 to 19 do begin
     BmPos := EditView.BookmarkPos[i];
     if BmPos.Line <> 0 then
-      FBookmarks.Add(i, BmPos.Line);
+      FBookmarks.Add(i, BmPos.Line, BmPos.CharIndex);
   end;
 end;
 
@@ -163,7 +85,7 @@ begin
   try
     for i := 0 to FBookmarks.Count - 1 do begin
       BmEditPos.Line := FBookmarks[i].Line;
-      BmEditPos.Col := 1;
+      BmEditPos.Col := FBookmarks[i].CharIdx;
       EditView.SetCursorPos(BmEditPos);
       EditView.BookmarkToggle(FBookmarks[i].Number);
     end;
