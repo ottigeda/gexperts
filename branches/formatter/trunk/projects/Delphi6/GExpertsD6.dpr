191cecb5132d320ff6ee0d032ba407f7ddaf6764
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2016-07-16 10:49:49 +0000

    added a dialog to select what to print
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@1680 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- branches/formatter/trunk/projects/Delphi6/GExpertsD6.dpr
+++ branches/formatter/trunk/projects/Delphi6/GExpertsD6.dpr
@@ -1,7 +1,6 @@
 library GExpertsD6;
 
 {$R '..\..\images\GXIcons.res' '..\..\images\GXIcons.rc'}
-{$R *_version.res}
 {$E dll}
 
 uses
@@ -209,7 +208,8 @@ uses
   GX_VCLFixPack in '..\..\Source\Utils\GX_VCLFixPack.pas',
   GX_dzVersionInfo in '..\..\Source\Utils\GX_dzVersionInfo.pas',
   GX_dzOsUtils in '..\..\Source\Utils\GX_dzOsUtils.pas',
-  GX_dzPackageInfo in '..\..\Source\Utils\GX_dzPackageInfo.pas';
+  GX_dzPackageInfo in '..\..\Source\Utils\GX_dzPackageInfo.pas',
+  GX_PeInfoPrint in '..\..\Source\PeInfo\GX_PeInfoPrint.pas' {f_PeInfoPrint};
 
 end.
 
