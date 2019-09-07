1a3c0e116ed975383a15242924cd556677fab741
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2019-09-07 14:37:31 +0000

    additional, more complex test for anonymous methods
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@2719 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ trunk/UnitTests/Testcases/input/testfile_Anonymous2.pas
@@ -0,0 +1,18 @@
+unit testfile_Anonymous;
+
+interface
+
+implementation
+
+procedure SomeProc;
+var
+  s: string;
+begin
+  RegisterConverter(TStringList,
+    function: integer
+    var b: boolean;
+    begin
+      bla;
+        blub;
+    end);
+end;
