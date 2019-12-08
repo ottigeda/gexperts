732215ca4362a323219491d09b969203ab322366
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2019-12-08 14:44:19 +0000

    extended assert tracing
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@2913 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/Source/Utils/GX_dzAssertTrace.pas
+++ trunk/Source/Utils/GX_dzAssertTrace.pas
@@ -3,10 +3,14 @@ unit GX_dzAssertTrace;
 interface
 
 uses
-  SysUtils;
+  SysUtils,
+  Classes;
 
 var
   gblAssertTraceOn: Boolean = False;
+
+function WriteTrace(const _fn: string): Boolean;
+function ClearTrace: Boolean;
 
 implementation
 
@@ -17,13 +21,57 @@ uses
 
 {$MESSAGE warn 'Assert tracing is turned on, this will impact performance!'}
 
+var
+  Trace: TStringList = nil;
+
 procedure DebugAssertLine(const _Message, _Filename: string; _LineNumber: Integer; _ErrorAddr: Pointer);
+var
+  Line: string;
 begin
-  if gblAssertTraceOn then
-    SendDebug(ChangeFileExt(ExtractFileName(_Filename), '') + ':' + IntToStr(_LineNumber) + ' ' + _Message);
+  if gblAssertTraceOn then begin
+    if not Assigned(Trace) then
+      Trace := TStringList.Create;
+    Line := ChangeFileExt(ExtractFileName(_Filename), '') + ':' + IntToStr(_LineNumber) + ' ' + _Message;
+//    SendDebug(Line);
+    Trace.Add(Line);
+  end;
+end;
+{$ENDIF}
+
+function WriteTrace(const _fn: string): Boolean;
+begin
+{$IF Declared(Trace)}
+  Result := (_fn <> '') and Assigned(Trace);
+  if Result then begin
+    Trace.SaveToFile(_fn);
+  end;
+{$ELSE}
+  Result := False;
+{$IFEND}
 end;
 
+function ClearTrace: Boolean;
+begin
+{$IF Declared(Trace)}
+  if Assigned(Trace) then
+    Trace.Clear;
+  Result := True;
+{$ELSE}
+  Result := True;
+{$IFEND}
+end;
+
+procedure dummy;
+begin
+  if ClearTrace then
+    WriteTrace('');
+end;
+
+{$IFDEF ASSERT_TRACING}
 initialization
+  dummy;
   AssertErrorProc := DebugAssertLine;
+finalization
+  FreeAndNil(Trace);
 {$ENDIF}
 end.
