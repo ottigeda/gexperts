e8eac18da2205051cbcde5593c2a89b0fa007f40
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2019-12-29 12:09:17 +0000

    added version information
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@2970 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/Projects/StandAlone/PeInformation/GExpertsPeInformation.dpr
+++ trunk/Projects/StandAlone/PeInformation/GExpertsPeInformation.dpr
@@ -5,7 +5,19 @@ uses
   GX_PeInfo in 'src\GX_PeInfo.pas',
   GX_PeInformation in 'src\GX_PeInformation.pas' {fmPeInformation},
   GX_PeInfoPrint in 'src\GX_PeInfoPrint.pas' {f_PeInfoPrint},
-  GX_SharedImages in 'src\GX_SharedImages.pas' {dmSharedImages: TDataModule};
+  GX_SharedImages in 'src\GX_SharedImages.pas' {dmSharedImages: TDataModule},
+  GX_GenericUtils in '..\..\..\Source\Utils\GX_GenericUtils.pas',
+  GX_DbugIntf in '..\..\..\Source\Utils\GX_DbugIntf.pas',
+  GX_Debug in '..\..\..\Source\Utils\GX_Debug.pas',
+  GX_dzSelectDirectoryFix in '..\..\..\Source\Utils\GX_dzSelectDirectoryFix.pas',
+  GX_dzVclUtils in '..\..\..\Source\Utils\GX_dzVclUtils.pas',
+  GX_dzOsUtils in '..\..\..\Source\Utils\GX_dzOsUtils.pas',
+  GX_dzVersionInfo in '..\..\..\Source\Utils\GX_dzVersionInfo.pas',
+  GX_dzPackageInfo in '..\..\..\Source\Utils\GX_dzPackageInfo.pas',
+  GX_dzClassUtils in '..\..\..\Source\Utils\GX_dzClassUtils.pas',
+  GX_dzNamedThread in '..\..\..\Source\Utils\GX_dzNamedThread.pas';
+
+{$R *_version.res}
 
 begin
   Application.Initialize;
