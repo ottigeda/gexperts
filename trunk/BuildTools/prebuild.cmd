3f04b9964e5ff69616ca05df3684ac01c5d17974
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2019-12-29 13:57:11 +0000

    * bugfix: parameters to prepbuild.exe changed to %PROJECTNAMEONLY% (must not include extension) * updated to also compile icon and update and compile manifest resources if they exist
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@2972 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/BuildTools/prebuild.cmd
+++ trunk/BuildTools/prebuild.cmd
@@ -5,21 +5,49 @@
 @rem Path\to\buildtools\prebuild.cmd $(OUTPUTDIR)$(OUTPUTNAME)
 @echo %0
 @echo running in %CD%
-
 set PROJECTPATH=%1
 rem remove quotes
 set PROJECTPATH=%PROJECTPATH:"=%
+rem echo PROJECTPATH=%PROJECTPATH%
 
 if "%PROJECTPATH%"=="" goto NeedPara
 rem echo PROJECTPATH=%PROJECTPATH%
 set PROJECTNAMEONLY=%~dpn1
 rem echo PROJECTNAMEONLY=%PROJECTNAMEONLY%
+
 set OUTPUTDIR=%~dp1
+rem echo OUTPUTDIR=%OUTPUTDIR%
 
 pushd %OUTPUTDIR%
-
-"%~dp0\prepbuild.exe" --readini="%PROJECTPATH%" --WriteRc="%PROJECTPATH%" --incbuild
+rem echo calling prepbuild.exe
+"%~dp0\prepbuild.exe" --incbuild --readini="%PROJECTNAMEONLY%" --WriteRc="%PROJECTNAMEONLY%" --InputManifest="%PROJECTNAMEONLY%.manifest.in" --manifest="%PROJECTNAMEONLY%" --updatemanifest --WriteManifestRc="%PROJECTNAMEONLY%" --ignoremanifesterrors
 brcc32 "%PROJECTNAMEONLY%_Version.rc"
+
+echo checking for mainfest.rc
+if not exist %PROJECTNAMEONLY%_Manifest.rc goto nomanirc
+echo manifest.rc file found
+
+echo checking for .manifest
+if not exist %PROJECTNAMEONLY%.manifest goto nomani
+echo .manifest file found
+brcc32 %PROJECTNAMEONLY%_Manifest.rc
+goto donemani
+:nomanirc
+echo Hint: %PROJECTNAMEONLY%_Manifest.rc not found, skipping
+goto donemani
+:nomani
+echo Hint: %PROJECTNAMEONLY%.manifest not found, skipping
+goto donemani
+:donemani
+
+echo checking for icon.rc
+if not exist %PROJECTNAMEONLY%_Icon.rc goto noicon
+echo icon.rc file found
+%~dp0\rc %PROJECTNAMEONLY%_Icon.rc
+goto doneicon
+:noicon
+echo Hint: %PROJECTNAMEONLY%_Icon.rc not found, skipping
+:doneicon
 
 popd
 
