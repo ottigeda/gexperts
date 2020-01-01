724f96f0d3fb29695eeedbf23d1e71a9566d5d82
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2020-01-01 07:26:54 +0000

    some more diagnostic output
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3010 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/BuildTools/movedll.cmd
+++ trunk/BuildTools/movedll.cmd
@@ -1,8 +1,12 @@
 @setlocal enableextensions
 @if not defined gx_cmd_debug (echo off)
 @endlocal
+@echo %0
+@echo running in %CD%
+@echo checking for %1
 if not exist %1 goto skipit
 echo moving %1 to %1~ so the DLL can be compiled even though it is currently loaded in the IDE
 move %1 %1~
 :skipit
+echo %0 exiting
 exit /b 0
