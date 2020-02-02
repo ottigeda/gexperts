61e30ccda4a3c41cc3895d53a8ed41fdde3de5e2
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2020-02-02 15:48:07 +0000

    * if the shift key is down while loading the DLL, show which one has been loaded * switched to release build * added version info and manifest
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3049 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/Projects/StandAlone/Grep/GExpertsGrep.dpr
+++ trunk/Projects/StandAlone/Grep/GExpertsGrep.dpr
@@ -1,12 +1,19 @@
 program GExpertsGrep;
 
-{$R *_version.res}
-{$R *_icon.res}
-{$R *_manifest.res}
+
 
 uses
   GX_StandAloneLoadDLL in '..\..\..\Source\Framework\GX_StandAloneLoadDLL.pas',
-  GrepMain in 'GrepMain.pas';
+  GrepMain in 'GrepMain.pas',
+  u_dzOsUtils in '..\..\..\ExternalSource\dzlib\u_dzOsUtils.pas',
+  u_dzTranslator in '..\..\..\ExternalSource\dzlib\u_dzTranslator.pas',
+  u_dzMiscUtils in '..\..\..\ExternalSource\dzlib\u_dzMiscUtils.pas',
+  u_dzFileUtils in '..\..\..\ExternalSource\dzlib\u_dzFileUtils.pas',
+  u_dzStringUtils in '..\..\..\ExternalSource\dzlib\u_dzStringUtils.pas',
+  u_dzTypes in '..\..\..\ExternalSource\dzlib\u_dzTypes.pas',
+  u_dzConvertUtils in '..\..\..\ExternalSource\dzlib\u_dzConvertUtils.pas',
+  u_dzDateUtils in '..\..\..\ExternalSource\dzlib\u_dzDateUtils.pas',
+  u_dzClassUtils in '..\..\..\ExternalSource\dzlib\u_dzClassUtils.pas';
 
 begin
   Main;
