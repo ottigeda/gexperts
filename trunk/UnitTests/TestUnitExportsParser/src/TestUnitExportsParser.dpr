9f12ef87a18ca14ed9767a145984bcf385e4931f
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2020-11-14 17:23:55 +0000

    Unit tests for the UnitExportParser
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3378 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ trunk/UnitTests/TestUnitExportsParser/src/TestUnitExportsParser.dpr
@@ -0,0 +1,30 @@
+program TestUnitExportsParser;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  GX_UnitExportsParserTest in 'GX_UnitExportsParserTest.pas',
+  GX_UnitExportsParser in '..\..\..\Source\Framework\GX_UnitExportsParser.pas',
+  DUnitConsts in '..\..\dunit\DUnitConsts.pas',
+  GUITestRunner in '..\..\dunit\GUITestRunner.pas' {GUITestRunner},
+  TestFramework in '..\..\dunit\TestFramework.pas',
+  TestFrameworkExt in '..\..\dunit\TestFrameworkExt.pas';
+
+{$R *.RES}
+
+begin
+  GUITestRunner.RunRegisteredTests;
+end.
+
