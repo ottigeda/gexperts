0ecdb90b859e5d47e46c59a5a7e9ee9ac2e5943d
Daniel Ottiger <daniel.ottiger@schindler.com>
2021-09-11 19:01:14 +0200

    avoid conflict with different units called 'Actions'

--- trunk/ExternalSource/dzlib/u_dzVclUtils.pas
+++ trunk/ExternalSource/dzlib/u_dzVclUtils.pas
@@ -31,7 +31,7 @@ uses
   Menus,
   MultiMon, // this unit doesn't exist in older Delphi versions, use a unit alias like Multimon=Windows in that case
 {$IFDEF HAS_UNIT_SYSTEM_ACTIONS}
-  Actions,
+  System.Actions,
 {$ENDIF}
 {$IFDEF HAS_UNIT_SYSTEM_UITYPES}
   UITypes,
