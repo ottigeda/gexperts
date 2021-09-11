d112566d8b04b1177a72f18601db74c5edbc080f
Daniel Ottiger <daniel.ottiger@schindler.com>
2021-09-11 18:42:32 +0200

    Delphi 11.0 - WiX based setup
    The binaries/dlls must be build manually first.
    Then run 11.0.bat to create a simple msi using WiX (wixtoolset.org).
    Only works for the latest version of the Delphi IDE.

---
+++ trunk/11.0.bat
@@ -0,0 +1,2 @@
+candle -dPRODUCT_ID={53034A06-3CA5-4E17-BE06-B4D48EFE0A4B} -dID= -dIDE_VERSION=110 -dIDE_NUMBER=22.0 -dVERSION=1.3.18 GExperts.wxs -out GExpertsRS110.wixobj
+light -ext WiXUtilExtension GExpertsRS110.wixobj
