a5eb5189d3712de794d4f6312658abd45972dc29
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2022-03-19 17:59:00 +0000

    * GX_DELPHI_11_UP -> GX_DELPHI11_UP * removed the STRING_GRID_OWNERDRAW_FIX_ENABLED cond. define because that fix is becoming more and more complex * The string grid OwnerDraw fix now should cover all supported Delphi versions.
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3823 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/Source/Utils/GX_StringGridDrawFix.pas
+++ trunk/Source/Utils/GX_StringGridDrawFix.pas
@@ -67,13 +67,28 @@ begin
 {$ENDIF}
 
   cnv := _sg.Canvas;
+  // todo: FillRect might also need to use other offsets apparently since Delphi XE2
   cnv.FillRect(_Rect);
-{$IFDEF STRING_GRID_OWNERDRAW_FIX_ENABLED}
-  // todo: Make a special cond. define for Delphi 11 (and possibly later)
-  //       so we don't need to call GetBorlandIdeVersion
-  if GetBorlandIdeVersion in [ideRS104U1, ideRS104U2, ideRS11] then begin
+{$IFNDEF GX_DELPHIXE2_UP}
+  // Versions before XE2 worked fine with an offset of 2 for both, x and y
+  cnv.TextRect(_Rect, _Rect.Left + 2, _Rect.Top + 2, _Text);
+{$ELSE}
+{$IFNDEF GX_DELPHI10_4_UP}
+  // Delphi XE2 up to 10.3 required an offset of 2 for x and 1 for y
+  cnv.TextRect(_Rect, _Rect.Left + 2, _Rect.Top + 1, _Text);
+{$ELSE}
+{$IFDEF GX_DELPHI11_UP}
+  // Delphi 11 is again broken differently:
+  // We still need an offset of 6 for x but again an offset of 2 for y
+  // (Maybe that is only the case for Delphi 11.1. I can't check for Delphi 11.0 because
+  // I have already updated.)
+  cnv.TextRect(_Rect, _Rect.Left + 6, _Rect.Top + 2, _Text)
+{$ELSE}
+  // so, now to the completely broken versions:
+  // In Delphi 10.4, up to patch 2 owner drawn StringGrids were broken
+  if GetBorlandIdeVersion in [ideRS104U1, ideRS104U2] then begin
     // in RS 10.4 Update 1 they apparently fixed the fix (or at least made it consistently wrong again)
-    cnv.TextRect(_Rect, _Rect.Left + 6, _Rect.Top + 2, _Text)
+    cnv.TextRect(_Rect, _Rect.Left + 6, _Rect.Top + 1, _Text)
   end else if GetBorlandIdeVersion in [ideRS104P2] then begin
     // Embarcadero managed to bungle the StringGrid redraw fix in RS 10.4 patch 2.
     // Now we have to check whether the grid is focused and use a different x offset in that case.
@@ -85,9 +100,9 @@ begin
     // fix for original bug
     cnv.TextRect(_Rect, _Rect.Left, _Rect.Top, _Text);
   end;
-{$ELSE}
-  cnv.TextRect(_Rect, _Rect.Left + 2, _Rect.Top + 2, _Text);
-{$ENDIF}
+{$ENDIF GX_DELPHI11_UP}
+{$ENDIF GX_DELPHI10_4_UP}
+{$ENDIF  GX_DELPHIXE2_UP}
 end;
 
 end.
