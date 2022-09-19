ec48da8920cf20ada5e1ba1f238e41bdcdd51e02
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2022-09-19 09:40:40 +0000

    new test case for bug #295 sign of negative numbers in Record constants is formatted incorrectly
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3889 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ trunk/UnitTestsFormatter/Testcases/input/testfile_SignInRecordConstant.pas
@@ -0,0 +1,18 @@
+unit testfile_SignInRecordConstant;
+
+interface
+
+type
+  TMyRec = record
+	  a: integer;
+		b: integer;
+	end;
+
+const
+  MyConst: TMyRec = (
+	  a: -1;
+		b: -5);
+
+implementation
+
+end.
