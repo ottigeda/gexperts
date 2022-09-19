dfbd7b88e8cc889e980df27d7e06b9024a402b54
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2022-09-19 09:47:39 +0000

    tab to spaces
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3890 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/UnitTestsFormatter/Testcases/input/testfile_SignInRecordConstant.pas
+++ trunk/UnitTestsFormatter/Testcases/input/testfile_SignInRecordConstant.pas
@@ -4,14 +4,14 @@ interface
 
 type
   TMyRec = record
-	  a: integer;
-		b: integer;
-	end;
+    a: integer;
+    b: integer;
+  end;
 
 const
   MyConst: TMyRec = (
-	  a: -1;
-		b: -5);
+    a: -1;
+    b: -5);
 
 implementation
 
