e1208d19965fd756d04092ce6775356844e47b30
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2023-01-01 14:57:22 +0000

    * implementation for feature request #153: Form Hotkeys expert should allow assigning hotkeys * other small improvements for that expert
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@3938 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/Source/FormHotkeys/GX_FormHotkeys.dfm
+++ trunk/Source/FormHotkeys/GX_FormHotkeys.dfm
@@ -4,64 +4,59 @@ object f_FormHotkeys: Tf_FormHotkeys
   BorderIcons = [biSystemMenu, biMaximize]
   Caption = 'Form Hotkeys'
   ClientHeight = 329
-  ClientWidth = 633
+  ClientWidth = 784
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -11
   Font.Name = 'Tahoma'
   Font.Style = []
-  DesignSize = (
-    633
-    329)
+  OldCreateOrder = False
+  OnResize = FormResize
   PixelsPerInch = 96
   TextHeight = 13
-  object l_HotAndAcceleratorKeys: TLabel
-    Left = 8
-    Top = 8
-    Width = 126
-    Height = 13
-    Caption = '&Hot- and Accelerator Keys'
-  end
   object TheGrid: TStringGrid
-    Left = 8
-    Top = 24
-    Width = 617
-    Height = 297
-    Anchors = [akLeft, akTop, akRight, akBottom]
-    ColCount = 6
+    Left = 0
+    Top = 0
+    Width = 784
+    Height = 329
+    Align = alClient
+    ColCount = 7
     DefaultColWidth = 50
     DefaultRowHeight = 20
     FixedCols = 0
     RowCount = 2
     Options = [goFixedVertLine, goFixedHorzLine, goVertLine, goHorzLine, goColSizing, goRowSelect]
+    PopupMenu = pm_Grid
     TabOrder = 0
     OnDrawCell = TheGridDrawCell
-  end
-  object b_somebutton: TButton
-    Left = 336
-    Top = 0
-    Width = 75
-    Height = 25
-    Action = act_
-    TabOrder = 1
   end
   object TheActionList: TActionList
     Left = 312
     Top = 176
-    object act_: TAction
-      Caption = '&Action1'
-      ShortCut = 16450
+    object act_AssignShortcut: TAction
+      Caption = '&Assign Shortcut'
+      ShortCut = 16449
       SecondaryShortCuts.Strings = (
         'Alt+Ctrl+X'
         'Shift+Alt+Ctrl+B')
+      OnExecute = act_AssignShortcutExecute
+    end
+    object act_GotoAction: TAction
+      Caption = '&Goto Action'
+      ShortCut = 16455
+      OnExecute = act_GotoActionExecute
     end
   end
-  object pm_: TPopupMenu
+  object pm_Grid: TPopupMenu
+    OnPopup = pm_GridPopup
     Left = 408
     Top = 136
-    object SomeMenuItem1: TMenuItem
-      Action = act_
+    object pm_AssingShortcut: TMenuItem
+      Action = act_AssignShortcut
+    end
+    object mi_GotoAction: TMenuItem
+      Action = act_GotoAction
     end
   end
 end
--- trunk/Source/FormHotkeys/GX_FormHotkeys.pas
+++ trunk/Source/FormHotkeys/GX_FormHotkeys.pas
@@ -31,6 +31,9 @@ type
     Accelerator: string;
     Shortcut: string;
     SecondaryShortcuts: TStringArray;
+    Action: string;
+    HasShortcutProperty: Boolean;
+    HasSecodaryShortcutProperty: Boolean;
   end;
 
 type
@@ -38,19 +41,23 @@ type
 
 type
   Tf_FormHotkeys = class(TfmBaseForm)
-    l_HotAndAcceleratorKeys: TLabel;
     TheGrid: TStringGrid;
     TheActionList: TActionList;
-    act_: TAction;
-    pm_: TPopupMenu;
-    SomeMenuItem1: TMenuItem;
-    b_somebutton: TButton;
+    act_AssignShortcut: TAction;
+    pm_Grid: TPopupMenu;
+    pm_AssingShortcut: TMenuItem;
+    act_GotoAction: TAction;
+    mi_GotoAction: TMenuItem;
     procedure TheGridDrawCell(Sender: TObject; ACol, ARow: Integer; Rect: TRect;
       State: TGridDrawState);
     procedure TheGridMouseWheelDown(Sender: TObject; Shift: TShiftState; MousePos: TPoint;
       var Handled: Boolean);
     procedure TheGridMouseWheelUp(Sender: TObject; Shift: TShiftState; MousePos: TPoint;
       var Handled: Boolean);
+    procedure act_AssignShortcutExecute(Sender: TObject);
+    procedure pm_GridPopup(Sender: TObject);
+    procedure act_GotoActionExecute(Sender: TObject);
+    procedure FormResize(Sender: TObject);
   private
     FCompRecs: TComponentRecArr;
     function ScrollGrid(_Grid: TStringGrid; _Direction: Integer; _Shift: TShiftState;
@@ -77,7 +84,9 @@ uses
   GX_OtaUtils,
   GX_Experts,
   GX_GetIdeVersion,
-  GX_StringGridDrawFix;
+  GX_StringGridDrawFix,
+  ComCtrls,
+  GX_FormHotkeysSelect;
 
 type
   TFormHotkeysExpert = class(TGX_Expert)
@@ -88,6 +97,15 @@ type
     procedure Execute(Sender: TObject); override;
     function HasConfigOptions: Boolean; override;
   end;
+
+const
+  COL_NAME = 0;
+  COL_TYPE = 1;
+  COL_CAPTION = 2;
+  COL_ACCEL = 3;
+  COL_ACTION = 4;
+  COL_SHORTCUT = 5;
+  COL_SECONDARY = 6;
 
 { Tf_FormHotkeys }
 
@@ -100,7 +118,7 @@ var
     i: Integer;
   begin
     Result := True;
-    for i := 0 to UsedActions.count - 1 do
+    for i := 0 to UsedActions.Count - 1 do
       if UsedActions[i] = _Action then
         Exit; //==>
     Result := False;
@@ -127,12 +145,13 @@ var
 begin
   inherited;
 
-  TheGrid.Cells[0, 0] := 'Component';
-  TheGrid.Cells[1, 0] := 'Type';
-  TheGrid.Cells[2, 0] := 'Caption';
-  TheGrid.Cells[3, 0] := 'Accelerator';
-  TheGrid.Cells[4, 0] := 'Shortcut';
-  TheGrid.Cells[5, 0] := 'Secondary Shortcuts';
+  TheGrid.Cells[COL_NAME, 0] := 'Component';
+  TheGrid.Cells[COL_TYPE, 0] := 'Type';
+  TheGrid.Cells[COL_CAPTION, 0] := 'Caption';
+  TheGrid.Cells[COL_ACCEL, 0] := 'Accelerator';
+  TheGrid.Cells[COL_ACTION, 0] := 'Action';
+  TheGrid.Cells[COL_SHORTCUT, 0] := 'Shortcut';
+  TheGrid.Cells[COL_SECONDARY, 0] := 'Secondary Shortcuts';
 
   TGrid_SetNonfixedRowCount(TheGrid, 0);
   if not GxOtaTryGetCurrentFormEditor(FormEditor) then
@@ -144,13 +163,13 @@ begin
 
   UsedActions := TList.Create;
   try
-    MaxSecondaryShortcutCount := 0;
+    MaxSecondaryShortcutCount := 1;
     cnt := Root.GetComponentCount;
     SetLength(FCompRecs, Offset + cnt);
     for i := 0 to cnt - 1 do begin
       cmp := Root.GetComponent(i);
       // I found no working way to access the SecondaryShortCuts value via IOTAComponent, so
-      // I'm going through the nataive component
+      // I'm going through the native component
       NativeCmp := GxOtaGetNativeComponent(cmp);
       rec := @FCompRecs[Offset + i];
       rec.CompType := NativeCmp.ClassName;
@@ -177,11 +196,12 @@ begin
 
         if (NativeCmp is TBasicAction) and not IsActionUsed(NativeCmp) then begin
           // Actions cannot be called by their accelerator key unless they are assigned to a control
+          // todo: Is that really true? I seem to remember it differently.
           rec.Accelerator := 'N/A';
         end else begin
           s := Menus.GetHotkey(s);
           if s <> '' then
-            rec.Accelerator := 'Alt+' + s
+            rec.Accelerator := 'Alt+' + UpperCase(s)
           else
             rec.Accelerator := '';
         end;
@@ -192,25 +212,32 @@ begin
 
       if Assigned(Action) then begin
         rec.Shortcut := 'N/A';
+        rec.Action := TBasicAction(Action).Name;
       end else begin
+        rec.Action := '';
         if cmp.GetPropValueByName('ShortCut', Shortcut) then begin
           rec.Shortcut := Menus.ShortCutToText(Shortcut);
+          rec.HasShortcutProperty := True;
         end else begin
           rec.Shortcut := 'N/A';
+          rec.HasShortcutProperty := False;
         end;
 
         Info := TypInfo.GetPropInfo(NativeCmp.ClassInfo, 'SecondaryShortCuts');
         if Assigned(Info) and (Info.PropType^.Kind = tkClass) then begin
+          rec.HasSecodaryShortcutProperty := True;
           Obj := TypInfo.GetObjectProp(NativeCmp, Info);
           if Assigned(Obj) and (Obj is TStringList) then begin
             sl := TStringList(Obj);
-            SecondaryShortcutCount := sl.count;
+            SecondaryShortcutCount := sl.Count;
             SetLength(rec.SecondaryShortcuts, SecondaryShortcutCount);
             for j := 0 to SecondaryShortcutCount - 1 do
               rec.SecondaryShortcuts[j] := sl[j];
             if SecondaryShortcutCount > MaxSecondaryShortcutCount then
               MaxSecondaryShortcutCount := SecondaryShortcutCount;
           end;
+        end else begin
+          rec.HasSecodaryShortcutProperty := False;
         end;
       end;
     end;
@@ -225,13 +252,22 @@ begin
     TGrid_SetNonfixedColCount(TheGrid, MaxSecondaryShortcutCount + 5);
   for i := 0 to cnt - 1 do begin
     rec := @FCompRecs[i];
-    TheGrid.Cells[0, Offset + i] := rec.CompName;
-    TheGrid.Cells[1, Offset + i] := rec.CompType;
-    TheGrid.Cells[2, Offset + i] := rec.Caption;
-    TheGrid.Cells[3, Offset + i] := rec.Accelerator;
-    TheGrid.Cells[4, Offset + i] := rec.Shortcut;
-    for j := 0 to Length(rec.SecondaryShortcuts) - 1 do begin
-      TheGrid.Cells[5 + j, Offset + i] := rec.SecondaryShortcuts[j];
+    TheGrid.Cells[COL_NAME, Offset + i] := rec.CompName;
+    TheGrid.Cells[COL_TYPE, Offset + i] := rec.CompType;
+    TheGrid.Cells[COL_CAPTION, Offset + i] := rec.Caption;
+    TheGrid.Cells[COL_ACCEL, Offset + i] := rec.Accelerator;
+    TheGrid.Cells[COL_ACTION, Offset + i] := rec.Action;
+    TheGrid.Cells[COL_SHORTCUT, Offset + i] := rec.Shortcut;
+    for j := 0 to MaxSecondaryShortcutCount - 1 do begin
+      if not rec.HasSecodaryShortcutProperty then begin
+        TheGrid.Cells[COL_SECONDARY + j, Offset + i] := 'N/A';
+      end else begin
+        if j < Length(rec.SecondaryShortcuts) then begin
+          TheGrid.Cells[COL_SECONDARY + j, Offset + i] := rec.SecondaryShortcuts[j];
+        end else begin
+          TheGrid.Cells[COL_SECONDARY + j, Offset + i] := '';
+        end;
+      end;
     end;
   end;
 
@@ -246,6 +282,11 @@ begin
   inherited;
 end;
 
+procedure Tf_FormHotkeys.FormResize(Sender: TObject);
+begin
+  TGrid_Resize(TheGrid, [roUseGridWidth, roUseAllRows]);
+end;
+
 {$IFDEF IDE_IS_HIDPI_AWARE}
 procedure Tf_FormHotkeys.ApplyDpi(_NewDpi: Integer; _NewBounds: PRect);
 begin
@@ -256,6 +297,113 @@ begin
   TStringGrid_AdjustRowHeight(TheGrid);
 end;
 {$ENDIF}
+
+procedure Tf_FormHotkeys.pm_GridPopup(Sender: TObject);
+var
+  FormEditor: IOTAFormEditor;
+  r: Integer;
+  rec: PComponentRec;
+  IsAssignShortcutAllowed: Boolean;
+  IsGotoActionAllowed: Boolean;
+begin
+  IsAssignShortcutAllowed := False;
+  IsGotoActionAllowed := False;
+  if GxOtaTryGetCurrentFormEditor(FormEditor) then begin
+    r := TGrid_GetNonfixedRow(TheGrid);
+    if (r >= Low(FCompRecs)) and (r <= High(FCompRecs)) then begin
+      rec := @FCompRecs[r];
+      if (rec.Action = '') and rec.HasShortcutProperty then
+        IsAssignShortcutAllowed := True;
+      if rec.Action <> '' then
+        IsGotoActionAllowed := True;
+    end;
+  end;
+  act_AssignShortcut.Enabled := IsAssignShortcutAllowed;
+  act_GotoAction.Enabled := IsGotoActionAllowed;
+end;
+
+procedure Tf_FormHotkeys.act_AssignShortcutExecute(Sender: TObject);
+var
+  FormEditor: IOTAFormEditor;
+  Root: IOTAComponent;
+  cnt: Integer;
+  i: Integer;
+  rec: PComponentRec;
+  cmp: IOTAComponent;
+  NativeCmp: TComponent;
+  r: Integer;
+  CompName: string;
+  CompShortcut: TShortCut;
+begin
+  if not GxOtaTryGetCurrentFormEditor(FormEditor) then
+    Exit; //==>
+
+  r := TGrid_GetNonfixedRow(TheGrid);
+  if (r < Low(FCompRecs)) or (r > High(FCompRecs)) then
+    Exit; //==>
+
+  rec := @FCompRecs[r];
+  if (rec.Action <> '') or not rec.HasShortcutProperty then
+    Exit; //==>
+
+  CompName := rec.CompName;
+
+  Root := FormEditor.GetRootComponent;
+  cnt := Root.GetComponentCount;
+  for i := 0 to cnt - 1 do begin
+    cmp := Root.GetComponent(i);
+    NativeCmp := GxOtaGetNativeComponent(cmp);
+    if SameText(NativeCmp.Name, CompName) then begin
+      if rec.Shortcut <> 'N/A' then
+        CompShortcut := TextToShortCut(rec.Shortcut)
+      else
+        CompShortcut := 0;
+      if TfmFormHotkeysSelect.Execute(Self, CompName, CompShortcut) then begin
+        if cmp.SetPropByName('ShortCut', CompShortcut) then begin
+          if cmp.GetPropValueByName('ShortCut', CompShortcut) then begin
+            rec.Shortcut := ShortCutToText(CompShortcut);
+            TStringGrid_SetNonfixedCell(TheGrid, COL_SHORTCUT, r, rec.Shortcut);
+          end;
+        end;
+      end;
+      Exit; //==>
+    end;
+  end;
+end;
+
+procedure Tf_FormHotkeys.act_GotoActionExecute(Sender: TObject);
+var
+  FormEditor: IOTAFormEditor;
+  Root: IOTAComponent;
+  cnt: Integer;
+  i: Integer;
+  rec: PComponentRec;
+  cmp: IOTAComponent;
+  NativeCmp: TComponent;
+  r: Integer;
+begin
+  if not GxOtaTryGetCurrentFormEditor(FormEditor) then
+    Exit; //==>
+
+  r := TGrid_GetNonfixedRow(TheGrid);
+  if (r < Low(FCompRecs)) or (r > High(FCompRecs)) then
+    Exit; //==>
+
+  rec := @FCompRecs[r];
+  if rec.Action = '' then
+    Exit; //==>
+
+  Root := FormEditor.GetRootComponent;
+  cnt := Root.GetComponentCount;
+  for i := 0 to cnt - 1 do begin
+    cmp := Root.GetComponent(i);
+    NativeCmp := GxOtaGetNativeComponent(cmp);
+    if SameText(NativeCmp.Name, rec.Action) then begin
+      TGrid_SetNonfixedRow(TheGrid, i);
+      Exit; //==>
+    end;
+  end;
+end;
 
 procedure Tf_FormHotkeys.TheGridDrawCell(Sender: TObject; ACol, ARow: Integer; Rect: TRect;
   State: TGridDrawState);
@@ -269,10 +417,13 @@ begin
   if (gdSelected in State) and (not (gdFocused in State) or
     ([goDrawFocusSelected, goRowSelect] * sg.Options <> [])) then begin
     cnv.Brush.Color := clHighlight;
+    cnv.Font.Color := clHighlightText;
   end else if gdFixed in State then begin
-    cnv.Brush.Color := clBtnFace
+    cnv.Brush.Color := clBtnFace;
+    cnv.Font.Color := clWindowText;
   end else begin
     cnv.Brush.Color := sg.Color;
+    cnv.Font.Color := clWindowText;
   end;
   cnv.FillRect(Rect);
 
