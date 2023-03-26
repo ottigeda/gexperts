c1a3f98a5a1bd6ea0c58f5c5709f2f22d8142932
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2023-03-26 15:37:50 +0000

    * dropped "experimental-twm" * Changed prefix from "GX" to "GExperts"
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@4013 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

--- trunk/BuildInstall/MakeInstallers.cmd
+++ trunk/BuildInstall/MakeInstallers.cmd
@@ -12,7 +12,7 @@ set version=%result%
 rem set instver=13D
 set instver=%version%
 call :getdate
-set special=experimental-twm_%dateYYYYMMDD%
+set special=%dateYYYYMMDD%
 
 rem call :makeinst RS103 RS10.3
 rem pause
@@ -45,7 +45,7 @@ goto :eof
 set Delphi=%1
 set Prefix=%2
 if "%prefix%" == "" set Prefix=%Delphi%
-%iscc% GExperts.iss /d%Delphi% /dVersion=%version% /fGX%Prefix%_%instver%_%special%
+%iscc% GExperts.iss /d%Delphi% /dVersion=%version% /fGExperts_%Prefix%_%instver%_%special%
 rem %iscc% GExperts.iss /dDelphi2010 /dVersion=%version% /fGXRS2010-%instver%-experimental-twm-%date%.exe
 goto :eof
 
