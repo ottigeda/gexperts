4e8daea5a4e9fac81e78a1798bc601abebc4e2d1
Daniel Ottiger <daniel.ottiger@schindler.com>
2023-11-22 21:48:37 +0100

    Delphi 12.0: update msi build script
    version seems to be 1.3.23

--- trunk/11.0.bat
+++
@@ -1,2 +0,0 @@
-candle -dPRODUCT_ID={53034A06-3CA5-4E17-BE06-B4D48EFE0A4B} -dID= -dIDE_VERSION=110 -dIDE_NUMBER=22.0 -dVERSION=1.3.21 GExperts.wxs -out GExpertsRS110.wixobj
-light -ext WiXUtilExtension GExpertsRS110.wixobj
---
+++ trunk/12.0.bat
@@ -0,0 +1,2 @@
+candle -dPRODUCT_ID={203A2966-514C-46D4-BA62-91B65F514BE9} -dID= -dIDE_VERSION=120 -dIDE_NUMBER=23.0 -dVERSION=1.3.23 GExperts.wxs -out GExpertsRS120.wixobj
+light -ext WiXUtilExtension GExpertsRS120.wixobj
