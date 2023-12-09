6c9035a736cee6cc3e718db487e7a66e619e2d3c
twm <twm@03fc42d2-69ba-4c40-8a67-5bfb646c2f2c>
2023-12-09 11:40:59 +0000

    new unit test for hint directives (deprecated, library, platform and experimental)
    git-svn-id: https://svn.code.sf.net/p/gexperts/code@4118 03fc42d2-69ba-4c40-8a67-5bfb646c2f2c

---
+++ trunk/UnitTestsFormatter/Testcases/input/testfile_HintDirectives.pas
@@ -0,0 +1,32 @@
+unit HintDirectives;
+
+interface
+
+procedure OldProc(bla: blub); deprecated;
+procedure PlatformProc( blub: bla); platform;
+procedure LibraryProc( a:integer); library;
+procedure ExperimentalProc(x,y: string); experimental;
+
+function OldFunc(bla: blub): integer; deprecated;
+function PlatformFunc( blub: bla): word; platform;
+function LibraryFunc( a:integer): string; library;
+function ExperimentalFunc(x,y: string): TObject; experimental;
+
+type
+  TBla = class(TBlub)
+  strict private
+	procedure OldProc(bla: blub); deprecated;
+procedure PlatformProc( blub: bla); platform;
+procedure LibraryProc( a:integer); library;
+procedure ExperimentalProc(x,y: string); experimental;
+
+function OldFunc(bla: blub): integer; deprecated;
+function PlatformFunc( blub: bla): word; platform;
+function LibraryFunc( a:integer): string; library;
+function ExperimentalFunc(x,y: string): TObject; experimental;
+end;
+
+implementation
+
+end.
+
